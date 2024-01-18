2688e24085b7ae9999cc101abf01c7d42e843404
rocklinux9527 <120091528+rocklinux9527@users.noreply.github.com>
2024-01-18 17:34:20 +0800

    Update sys_app_template.py

--- service/sys_app_template.py
+++ service/sys_app_template.py
@@ -1,11 +1,13 @@
 import json
-from typing import Dict, Any, Union
+from typing import Dict, Any, Union, List
 
-from sql_app.ops_app_template import insert_db_app_template, updata_app_template, delete_db_app_template, query_app_template, query_Template_app_name
+from sql_app.ops_app_template import insert_db_app_template, updata_app_template, query_Template_app_name, \
+    delete_app_template
 from sql_app.ops_log_db_play import insert_ops_bot_log
 
 from datetime import datetime, timezone, timedelta
 from tools.config import setup_logger
+
 
 class AppTemplateService:
     @staticmethod
@@ -18,13 +20,6 @@ class AppTemplateService:
             return {"code": 20000, "message": f"Template {name} already exists", "status": True,
                     "data": "create Template failure"}
         return {}
-
-    @classmethod
-    def save_data_to_database(cls, transformed_data):
-        for data in transformed_data:
-            print("数据在这里")
-            print(data)
-            cls.add_controller_app_template(**data)
 
     @classmethod
     def add_controller_app_template(cls, name: str, used: str) -> Dict[str, Any]:
@@ -47,11 +42,14 @@ class AppTemplateService:
             logger.info("Create App Template Successfully  ", extra={'props': {"app_template_info": name}})
             print("Successfully inserted data:", name)
         else:
-            logger.info("Create App Template failure ", extra={'props': {"app_template_info": name }})
-            return {"code": 50000, "message": "app template  failure", "status": True, "data": " create app template failure"}
+            logger.info("Create App Template failure ", extra={'props': {"app_template_info": name}})
+            return {"code": 50000, "message": "app template  failure", "status": True,
+                    "data": " create app template failure"}
         return created_app_template
+
     @classmethod
-    def update_controller_app_template(cls, ID: int, name: str, used: str, user_request_data: Dict[str, Any]) -> Dict[str, Any]:
+    def update_controller_app_template(cls, ID: int, name: str, used: str, user_request_data: Dict[str, Any]) -> Dict[
+        str, Any]:
         """1.更新app 模板 """
         china_timezone = timezone(timedelta(hours=8))
         current_time = datetime.now(china_timezone)
@@ -64,15 +62,10 @@ class AppTemplateService:
             return {"code": 50000, "message": "app template update failure", "status": True, "data": "failure"}
 
     @classmethod
-    def delete_controller_app_template(cls, ID: int, name: str, user_request_data: Dict[str, Any]) -> Dict[str, Any]:
-        """1.删除app 模板"""
+    def delete_controller_app_template(cls, template_id: int, name: str, user_request_data: Dict[str, Any]) -> Dict[
+        str, Any]:
         result = AppTemplateService.check_template_app_name(name)
         if not result:
-            return {"code": 50000, "message": "template app delete failure", "status": True, "data": "failure"}
-
-        result = delete_db_app_template(ID)
-        if result.get("code") == 20000:
-            insert_ops_bot_log("Delete App Template success", json.dumps(user_request_data), "post", json.dumps(result))
-            return result
-        else:
-            return {"code": 50000, "message": "Delete Template failure", "status": True, "data": "failure"}
+            return {"code": 50000, "message": "app template delete  app template not exist ", "status": True,
+                    "data": "failure"}
+        return delete_app_template(template_id, user_request_data)
